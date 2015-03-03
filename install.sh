6983b644cf69559ae19649adf59929dc07ca0166
irxground <irxnjhtchlnrw@gmail.com>
2015-03-03 12:20:56 +0900

    Organize directory

--- install.sh
+++ install.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
+function make_link() {
+  local _src=$1
+  local _dst=$2
+
+  if [ "$_src" = "$(readlink "$_dst")" ]; then
+    echo "Skip: $_src"
+  else
+    printf "ln -i -s %s %s\n" "$_src" "$_dst"
+    ln -s "$_src" "$_dst"
+  fi
+}
+
 echo "*** Install dotfile ***"
 cd $(dirname $0)
-for i in user.*; do
-  src=${PWD}/$i
-  dst=${HOME}/$(echo $i | sed -e "s/^user//")
-  if [ "$src" = "$(readlink "$dst")" ]; then
-    echo "Skip: $dst"
-  else
-    printf "ln -i -s %s %s\n" "$src" "$dst"
-    ln -i -s "$src" "$dst"
-  fi
+for i in $(ls "dotfiles")
+do
+  make_link "${PWD}/dotfiles/$i" "${HOME}/.$i"
 done
 
 echo "*** Install bin ***"
 mkdir -p "${HOME}/bin"
-for i in $(ls "bin") ; do
-  src=${PWD}/bin/$i
-  dst=${HOME}/bin/$i
-  if [ "$src" = "$(readlink "$dst")" ]; then
-    echo "Skip: $dst"
-  else
-    printf "ln -i -s %s %s\n" "$src" "$dst"
-    ln -i -s "$src" "$dst"
-  fi
+for i in $(ls "bin")
+do
+  make_link "${PWD}/bin/$i" "${HOME}/bin/$i"
 done
 
