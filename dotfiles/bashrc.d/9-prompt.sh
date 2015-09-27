f68f1256c5f2b985e308dde115499cecf441462f
irxground <irxnjhtchlnrw@gmail.com>
2015-09-27 22:54:16 +0900

    update PS1

--- dotfiles/bashrc.d/9-prompt.sh
+++ dotfiles/bashrc.d/9-prompt.sh
@@ -1,1 +1,19 @@
-PS1='\[\033[32m\]\u\[\033[00m\]:\[\033[34m\]\w\[\033[00m\]\n\$ '
+__red='\e[31m'
+__green='\e[32m'
+__yellow='\e[33m'
+__blue='\e[34m'
+__cyan='\e[36m'
+__reset='\e[0m'
+__ps1_pre="${__green}\u${__reset}@${__yellow}\h${__reset}:${__cyan}\w${__reset}"
+__ps1_post="\n\$ "
+if has __git_ps1; then
+  GIT_PS1_SHOWDIRTYSTATE=1
+  GIT_PS1_SHOWUNTRACKEDFILES=1
+  GIT_PS1_SHOWUPSTREAM="verbose"
+  GIT_PS1_SHOWCOLORHINTS=1
+  PROMPT_COMMAND='__git_ps1 "$__ps1_pre" "$__ps1_post"'
+  PS1=""
+else
+  PROMPT_COMMAND=""
+  PS1="${__ps1_pre}${__ps1_post}"
+fi
