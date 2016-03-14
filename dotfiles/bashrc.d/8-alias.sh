60518945b3508e514adb0497ddddb29fc7074ae3
irxground <irxnjhtchlnrw@gmail.com>
2016-03-14 16:21:54 +0900

    update

--- dotfiles/bashrc.d/8-alias.sh
+++ dotfiles/bashrc.d/8-alias.sh
@@ -1,5 +1,8 @@
 alias ls="ls -G"
 alias ll="ls -al"
+alias diff="diff -u"
+alias vim-sjis="vim -c ':e ++enc=cp932'"
+alias colordiff="colordiff -u"
 alias git-ammend="git commit --amend --no-edit"
 alias git-delete-branch="git branch --merged|egrep -v '\\*|develop|master'|xargs git branch -d"
 
