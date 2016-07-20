0ed7b0b56460b692e94b02b0007bf2b2ac069c74
irxground <irxnjhtchlnrw@gmail.com>
2016-07-20 10:42:19 +0900

    update

--- dotfiles/bashrc.d/1-android-sdk.sh
+++ dotfiles/bashrc.d/1-android-sdk.sh
@@ -1,3 +1,9 @@
 if has brew && [ -x "$(brew --prefix)/bin/android" ]; then
   export ANDROID_HOME=$(brew --prefix)/opt/android-sdk
 fi
+
+if [ -z "$ANDROID_HOME" ] && [ -d "$HOME/Library/Android/sdk" ]; then
+  export ANDROID_HOME="$HOME/Library/Android/sdk"
+fi
+
+prepend_path "${ANDROID_HOME}/platform-tools"
